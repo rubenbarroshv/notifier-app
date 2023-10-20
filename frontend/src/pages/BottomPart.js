7d973e786527f32e108c3a402e2d1ad11d660548
Ruben Barros <ruben.barros@hitachivantara.com>
2023-10-20 17:08:08 +0100

    feat: table component soft

--- frontend/src/pages/BottomPart.js
+++ frontend/src/pages/BottomPart.js
@@ -2,12 +2,19 @@ import React from "react";
 import { HvContainer } from "@hitachivantara/uikit-react-core";
 
 const BottomPart = () => {
+    const header = "Cenas";
     const title = "Header";
     const content = "Footer";
     return (
         <HvContainer>
             <div>{title}</div>
-            <div>BottomPart</div>
+            <hr/>
+            <table>
+                <tr>{header}</tr>
+                <td>Cenas 2</td>
+                <td>Cenas 2</td>
+            </table>
+            <hr></hr>
             <div>{content}</div>
         </HvContainer>
     );

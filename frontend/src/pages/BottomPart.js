2bf551852c0cdcba9efe2572a6c6639abbfe84e5
Ruben Barros <ruben.barros@hitachivantara.com>
2023-10-20 11:37:12 +0100

    feat: add bottom part.

---
+++ frontend/src/pages/BottomPart.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HvContainer } from "@hitachivantara/uikit-react-core";
+
+const BottomPart = () => {
+    return (
+        <HvContainer>
+            <div>BottomPart</div>
+        </HvContainer>
+    );
+};
+
+export default BottomPart;
